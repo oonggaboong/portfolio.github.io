0b364c7496e6582c4464df3504d95bed935b292c
oonggaboong <43745237+oonggaboong@users.noreply.github.com>
2022-06-26 02:27:52 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,56 +1,56 @@
 // for fading effect
 
-window.addEventListener('scroll',()=>{
-  let content = document.querySelector('.fading');
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".fading");
   let contentPosition = content.getBoundingClientRect().top;
-  let screenPosition = window.innerHeight/1;
-  if(contentPosition < screenPosition){
-    content.classList.add('active');
-  }else{
-    content.classList.remove('active');
+  let screenPosition = window.innerHeight / 1;
+  if (contentPosition < screenPosition) {
+    content.classList.add("active");
+  } else {
+    content.classList.remove("active");
   }
-})
+});
 
-window.addEventListener('scroll',()=>{
-  let content = document.querySelector('.fading-1');
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".fading-1");
   let contentPosition = content.getBoundingClientRect().top;
-  let screenPosition = window.innerHeight/0.7;
-  if(contentPosition < screenPosition){
-    content.classList.add('active');
-  }else{
-    content.classList.remove('active');
+  let screenPosition = window.innerHeight / 0.7;
+  if (contentPosition < screenPosition) {
+    content.classList.add("active");
+  } else {
+    content.classList.remove("active");
   }
-})
+});
 
-window.addEventListener('scroll',()=>{
-  let content = document.querySelector('.fading-2');
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".fading-2");
   let contentPosition = content.getBoundingClientRect().top;
-  let screenPosition = window.innerHeight/1;
-  if(contentPosition < screenPosition){
-    content.classList.add('active');
-  }else{
-    content.classList.remove('active');
+  let screenPosition = window.innerHeight / 1;
+  if (contentPosition < screenPosition) {
+    content.classList.add("active");
+  } else {
+    content.classList.remove("active");
   }
-})
+});
 
-window.addEventListener('scroll',()=>{
-  let content = document.querySelector('.fading-3');
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".fading-3");
   let contentPosition = content.getBoundingClientRect().top;
-  let screenPosition = window.innerHeight/1;
-  if(contentPosition < screenPosition){
-    content.classList.add('active');
-  }else{
-    content.classList.remove('active');
+  let screenPosition = window.innerHeight / 0.8;
+  if (contentPosition < screenPosition) {
+    content.classList.add("active");
+  } else {
+    content.classList.remove("active");
   }
-})
+});
 
-window.addEventListener('scroll',()=>{
-  let content = document.querySelector('.fading-4');
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".fading-4");
   let contentPosition = content.getBoundingClientRect().top;
-  let screenPosition = window.innerHeight/1;
-  if(contentPosition < screenPosition){
-    content.classList.add('active');
-  }else{
-    content.classList.remove('active');
+  let screenPosition = window.innerHeight / 1;
+  if (contentPosition < screenPosition) {
+    content.classList.add("active");
+  } else {
+    content.classList.remove("active");
   }
-})
+});
